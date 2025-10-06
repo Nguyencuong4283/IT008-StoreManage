8acb8116f82e05a6fdc2ed3998cdafb7296f5c7f
Nguyễn Cường <24520237@gm.uit.edu.vn>
2025-10-06 21:21:24 +0700

    update

--- Store/ViewModels/MainWindowViewModel.cs
+++ Store/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,10 @@
 ﻿using Store.Views;
-using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace Store.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public object CurrentPage { get; set; }
-
-    public MainWindowViewModel()
-    {
-        CurrentPage = null;
-    }
-
     [RelayCommand]
     private void LogInButton()
     {
@@ -27,7 +19,7 @@ public partial class MainWindowViewModel : ViewModelBase
         {
             mainWindow.Close();
         }
-
+        
     }
     [RelayCommand]
     private void RegisterButton()
