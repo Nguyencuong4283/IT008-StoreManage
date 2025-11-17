37eff1ddb268452e915cc6f7a6876b84322e3c82
LeVanAn <levanan1902006@nan1902006gmail.06gmailcom>
2025-11-17 22:52:08 +0700

    update

--- Store/obj/Debug/net9.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net9.0/Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a9e1b43ed4bdb2ac8e2801157269a5086f81e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08154f7b16625dfe4a3753155232a066cff48d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
