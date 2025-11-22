c637bcef3d06dfd88e30b43ad472ded0670119f5
Nguyễn Cường <24520237@gm.uit.edu.vn>
2025-11-22 21:08:43 +0700

    thêm biều đồ cho trang thu nhập

--- Store/ViewModels/IncomePageViewModel.cs
+++ Store/ViewModels/IncomePageViewModel.cs
@@ -1,6 +1,39 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
 namespace Store.ViewModels;
 
-public class IncomePageViewModel : ViewModelBase
+public partial class IncomePageViewModel : ViewModelBase
 {
     public string Income { get; set; } = "Thu nhập";
+    
+    private readonly ObservableCollection<int> _values;
+    public ISeries[] _series { get; set; }
+    public Axis[] xAxis { get; set; }
+    public Axis[] yAxis { get; set; }
+    public IncomePageViewModel()
+    {
+        _values = new ObservableCollection<int> {0,10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        _series = new ISeries[]
+        {
+            new LineSeries<int>
+            {
+                Values = _values,
+                GeometrySize = 5,
+                LineSmoothness = 0
+            }
+        };
+
+        xAxis = new Axis[]
+        {
+            new Axis()
+            {
+                Labeler = _value => $"{_value}m",
+                LabelsRotation = 0
+            }
+        };
+
+    }
 }
