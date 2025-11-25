85940c1ad5aa3a55bb7cb95305c58b46a880412f
Nguyễn Cường <24520237@gm.uit.edu.vn>
2025-11-25 07:28:54 +0700

    chỉnh sửa UI

--- Store/Views/SettingPageView.axaml.cs
+++ Store/Views/SettingPageView.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
 
 namespace Store.Views;
 
@@ -13,5 +15,16 @@ public partial class SettingPageView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var app = Application.Current;
+        if (app is not null)
+        {
+            // Nếu đang là Dark thì chuyển sang Light, và ngược lại
+            app.RequestedThemeVariant =
+                app.RequestedThemeVariant == ThemeVariant.Dark ? ThemeVariant.Light : ThemeVariant.Dark;
+        }
     }
 }
