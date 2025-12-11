2aae94d3f05911beae298da57619fbddbc96b0d6
vucch1wdv <trandinhsang6767@gmail.com>
2025-12-11 17:21:44 +0700

    update Tìm kiếm Khách hàng, Sản phẩm

--- Store/obj/Debug/net9.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net9.0/Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30dd211f12da99586cf0b7731d5a015f897fb3fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0164c9c723aa33ea89436a4622500c7621c918f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
