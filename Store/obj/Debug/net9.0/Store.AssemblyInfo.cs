68cf14ff61a7e543655ce5e4e51ba4626fd3ead8
cuongnguyen <cuongnuyen4283@gmail.com>
2025-12-15 14:01:34 +0700

    Tesst.axaml

--- Store/obj/Debug/net9.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net9.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e193b10c090390cafa682408f1baaa4ed472a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a51d220b57eb6359ad18480771f89cdca7c4f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
