8c028c0fad54c0eaafb10d2f745adeddce713aad
cuongnguyen <cuongnuyen4283@gmail.com>
2025-12-20 10:07:29 +0700

    thêm 3 nút chức năng tắt, thu nhỏ cửa sổ , phóng to

--- Store/Views/Manager/AdminWindowView.axaml.cs
+++ Store/Views/Manager/AdminWindowView.axaml.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Store.ViewModels.Manager;
 
@@ -11,6 +13,11 @@ public partial class AdminWindowView : Window
         InitializeComponent();
         DataContext = new AdminWindowViewModel();
     }
+    
+    private Window? GetParentWindow(Control control)
+    {
+        return this.VisualRoot as Window;
+    }
 
     private void InitializeComponent()
     {
@@ -21,5 +28,42 @@ public partial class AdminWindowView : Window
         var mainWindow = new Auth.MainWindow();
         mainWindow.Show();
         this.Close(); // đóng AdminWindow
+    }
+
+    private void Header_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            // nhấn 2 lần để phóng to/thu nhỏ cửa sổ
+            this.WindowState = this.WindowState == WindowState.Maximized 
+                ? WindowState.Normal 
+                : WindowState.Maximized;
+        }
+        else
+        {
+            // Nếu nhấp một lần thì cho phép kéo cửa sổ
+            this.BeginMoveDrag(e);
+        }
+    }
+    
+    private void CloseBtn(object? sender, RoutedEventArgs e)
+    {
+        GetParentWindow(this)?.Close();
+    }
+    
+    private void MinimizeBtn(object? sender, RoutedEventArgs e)
+    {
+        GetParentWindow(this)!.WindowState = WindowState.Minimized;
+    }
+    
+    private void MaximizeBtn(object? sender, RoutedEventArgs e)
+    {
+        var window = GetParentWindow(this);
+        if (window != null)
+        {
+            window.WindowState = window.WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
     }
 }
